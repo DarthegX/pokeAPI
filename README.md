a399eaa031912ab40cac742bcc18303d516a1551
DarthegX <elenainternxt@gmail.com>
2022-02-14 13:34:08 +0100

    add swagger page

--- README.md
+++ README.md
@@ -6,14 +6,14 @@
 
   ```nvm install v15```
 
-# Install
+## Install
 
 - Clone repository with `git clone https://github.com/DarthegX/pokeAPI.git`
 
 - Use `npm install` to install project dependencies.
 
 
-#### Start app
+## Start app
 
 Run `npm start` to start server in production mode.
 
--- src/app.js
+++ src/app.js
@@ -1,19 +1,48 @@
 import express from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import indexRouter from './routes/index.js';
 
-
-/**
- * Instance of Express application
- */
 const server = express();
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-//this.server.use(cookieParser());
 server.use('/pokemon', indexRouter);
-server.listen(777, () => {
-    console.log('Poke Server started! :)\n');
-    console.log('By Elena Garrigos');
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "PokeAPI with Swagger",
+            version: "1.0.0",
+            description:
+                "A simple API for retrieving information about your favourites Pokemons",
+            contact: {
+                name: "Elena Garrigos",
+                email: "elenacpsafa@gmail.com",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:8080/pokemon/",
+            },
+        ],
+    },
+    apis: ["./routes/index.js"],
+};
+
+const specs = swaggerJsdoc(options);
+server.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
+
+server.listen(8080, () => {
+    console.log('\nPoke Server started! :)\n');
+    console.log('By Elena Garrigos\n\n');
+
+    console.log('Go to /docs for HowToUse information');
 });
 
 export default server;
