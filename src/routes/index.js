1b1ac09120cc284ad07c8d8902233578e8cb3edc
DarthegX <elenainternxt@gmail.com>
2022-02-17 08:36:01 +0100

    add cache memory

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import NodeCache from 'node-cache';
 import PokeService from '../services/index.js';
 
 const indexRouter = express.Router();
+const cache = new NodeCache();
 const pokeService = new PokeService();
 
             
@@ -22,7 +24,7 @@ indexRouter.get('/', (req, res) => {
 
 /** 
  * @swagger 
- * /{name}: 
+ * /:name: 
  *   get: 
  *     description: Get information about a pokemon
  *     url parameters: 
@@ -30,12 +32,25 @@ indexRouter.get('/', (req, res) => {
  *     responses:  
  *       200: 
  *         description: Success
+ *       500:
+ *         description: Server Error
  */
 indexRouter.get('/:name', (req, res) => {
   const { name } = req.params;
+
+  if(cache.has(name)) {
+    const pokemon = cache.get(name);
+
+    return res.status(200).json(pokemon);
+  }
+
   pokeService.getPokemonInformationByName(name).then((result) => {
-    return res.status(200).json({ result })
-  }).catch(err => res.status(500).json({ error: err }));
+    cache.set(name, result);
+
+    return res.status(200).json(result)
+  }).catch(err => {
+    res.status(500).json({ error: err })
+  });
 })
 
 export default indexRouter;
