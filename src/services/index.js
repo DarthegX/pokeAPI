4121838cb9bac228c3779ab5926b44c5908fe75c
DarthegX <elenainternxt@gmail.com>
2022-02-17 08:37:36 +0100

    add requests pagination

--- src/services/index.js
+++ src/services/index.js
@@ -9,19 +9,49 @@ class PokeService {
 
     getPokemonInformationByName(name) {
         const url = pokemonUrl + name;
-        return axios.get(url, {}, {}).then((response) => {
-            const requests = response.data.moves.map(m => this.getPokemonMoveById(m.move.url))
-            return Promise.all(requests).then((result) => {
-                const pokemon = {
-                    id: response.data.id,
-                    name: response.data.name,
-                    moves: result
-                }
-    
-                return pokemon;
-            })
+
+        return axios.get(url, {}, {}).then(async (response) => {
+            const movesResult = await this.getAllPokemonMoves(response.data.moves);
+            const moves = movesResult.flat();
+
+            const pokemon = {
+                id: response.data.id,
+                name: response.data.name,
+                moves
+            }
+
+            return pokemon;
         })
     }
+
+
+    async getAllPokemonMoves(moves) {
+        let page = 0;
+        let movesPage = moves.slice(page, page + 10);
+        let movesResult = await this.get10PokemonMovesById(movesPage);
+        let pokemonMoves = movesResult;
+
+        while(page < moves.length) {
+            page += 10;
+
+            movesPage = moves.slice(page, page + 10);
+            movesResult = await this.get10PokemonMovesById(movesPage);
+
+            pokemonMoves.push(movesResult)
+        }
+
+        return pokemonMoves;
+    }
+
+
+    get10PokemonMovesById(movesPage) {
+        const requests = movesPage.map(m => this.getPokemonMoveById(m.move.url));
+
+        return Promise.all(requests).then((result) => {
+            return result;
+        })
+    }
+
 
     getPokemonMoveById = (url) =>
         new Promise((resolve, reject) => {
