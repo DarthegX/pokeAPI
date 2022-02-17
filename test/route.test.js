867d6e3ce0fbeb9906fbac2833608a2e5be09f9b
DarthegX <elenainternxt@gmail.com>
2022-02-17 14:33:19 +0100

    add response data test

--- test/route.test.js
+++ test/route.test.js
@@ -1,7 +1,6 @@
 const request = require('supertest');
 import app  from '../src/app.js';
 
-
 describe("Test root path", () => {
   test('should response the GET method', () => {
     return request(app)
@@ -12,11 +11,36 @@ describe("Test root path", () => {
   });
 });
 
+
 describe('Test the Pokemon endpoint', () => {
-  test("should specify json as the content type in the http header", () => {
+  const pokemonName = 'ditto';
+
+  test("should contain pokemon name in the json response", () => {
     return request(app)
-      .get("/pokemon/:name").then(response => {
+      .get(`/pokemon/${pokemonName}`).then(response => {
+        expect(response.statusCode).toBe(200);
         expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
+
+        const responseJson = JSON.parse(response.text);
+        expect(responseJson.name).toEqual(pokemonName);
+      });
+  });
+});
+
+
+describe('Test the Pokemon endpoint', () => {
+  const pokemonName = 'ditto';
+
+  test("should return a valid id", () => {
+    return request(app)
+      .get(`/pokemon/${pokemonName}`).then(response => {
+        expect(response.statusCode).toBe(200);
+        expect(response.headers['content-type']).toEqual(expect.stringContaining('json'));
+
+        const responseJson = JSON.parse(response.text);
+
+        expect(responseJson.id).toBeGreaterThanOrEqual(1);
+        expect(responseJson.id).toBeLessThanOrEqual(1118)
       });
   });
 });
